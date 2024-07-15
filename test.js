c962aa4e35199d540a195f01ab4a4103cd85be8b
Teju29-eng <tejeswiniarika29@gmail.com>
2024-07-15 13:46:05 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,32 +1,9 @@
-/*function sum(a, b) {
+function sum(a, b) {
     return a + b;
   }
  
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
- */
+ 
 
-{
-  "name": "my-node-app",
-  "version": "1.0.0",
-  "description": "A sample Node.js application",
-  "main": "index.js",
-  "scripts": {
-     "test": "echo \"Error: no test specified\""
-  },
-  "keywords": [
-    "node",
-    "sample",
-    "example"
-  ],
-  "author": "Your Name",
-  "license": "ISC",
-  "dependencies": {
-    "express": "^4.17.1"
-  },
-  "devDependencies": {
-    "mocha": "^9.1.3",
-    "chai": "^4.3.4"
-  }
-}
